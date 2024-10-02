54cba96bffd2e01cd98a2e6d4890bc550f6b79c2
Mc Villalobos <mcstalin421@gmail.com>
2024-10-02 14:37:34 +0800

    asd

--- server/index.js
+++ server/index.js
@@ -18,13 +18,15 @@ app.use(express.json({ limit : "50mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/requirements', express.static(path.join(__dirname, 'requirements')));
+
+// router 
+app.use('/', require('./src/Routes/ApiRoutes'));
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-// router 
-app.use('/', require('./src/Routes/ApiRoutes'));
 
 // database connection
 mongoose.connect(process.env.MONGODB_URI)
