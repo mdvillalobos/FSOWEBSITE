da383debdbf5eb1bd97ae400881faaf8f53f04fb
Mc Villalobos <mcstalin421@gmail.com>
2024-10-02 17:53:09 +0800

    asd

--- server/src/Middleware/uploadMiddleware.js
+++ server/src/Middleware/uploadMiddleware.js
@@ -28,31 +28,27 @@ const upload = multer({
     limits: { fileSize: 50 * 1024 * 1024 }
 });
 
-const multerErrorHandler = (req, res, next) => {
-    const start = Date.now();
-    upload.fields([{ name: 'requirement_1', maxCount: 1}, { name: 'requirement_2', maxCount: 1}, 
-        { name: 'requirement_3', maxCount: 1}, { name: 'requirement_4', maxCount: 1 }, { name: 'requirement_5', maxCount: 1}, 
-        { name: 'requirement_6', maxCount: 1}, { name: 'requirement_7', maxCount: 1 }, { name: 'requirement_8', maxCount: 1}, 
-        { name: 'requirement_9', maxCount: 1}, { name: 'requirement_10', maxCount: 1}, 
-    ])(req, res, (err) => {
-        if (err.message === 'Error: File type not supported!') {
-            console.log(`Sending Invalid File Type Error: ${err.message}`);
-            return res.status(400).json({ error: 'invalid file type' });
-            console.log(`General Error Sended: ${ err.message }`);
-        }
 
-        if(err instanceof multer.MulterError) {
-            console.log(`Sending Multer Error: ${err.message}`);
-            res.json({ error: err.message })
-            console.log(`Multer Error Sended: ${ err.message }`);
-            console.log(`MUlter Error Timeout at: ${ Date.now() - start }ms`)
-            return;
-        }
-    
-        console.log('Go to the next Step');
-        console.log(`Multer Error Handler run at: ${ Date.now() - start }ms`);
-        next();
-    })
+const multerErrorHandler = (err, req, res, next) => {
+    const start = Date.now();
+    if(err instanceof multer.MulterError) {
+        console.log(`Sending Multer Error: ${err.message}`);
+        res.json({ error: err.message })
+        console.log(`Multer Error Sended: ${ err.message }`);
+        console.log(`MUlter Error Timeout at: ${ Date.now() - start }ms`)
+        return;
+    }
+    else if(err) {
+        console.log(`Sending General Error: ${err.message}`);;
+        res.json({ error: err.message})
+        console.log(`General Error Sended: ${ err.message }`);
+        console.log(`General Error Timeout at: ${ Date.now() - start }ms`)
+        return
+    }
+
+    console.log('Go to the next Step');
+    console.log(`Multer Error Handler run at: ${ Date.now() - start }ms`);
+    next();
 }
 
 module.exports = {
