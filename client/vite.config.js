ad8e7c1ec9fa66432a3dbaec2bb735ec729464ff
Mc Villalobos <mcstalin421@gmail.com>
2024-10-14 22:24:49 +0800

    asd

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react()
   ],
-  build: {
-    lib: {
-      entry: './src/main.jsx',
-      fileName: 'my-app',
-      formats: ['es'],
-    },
-  },
 })
--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ app.use('/', apiRoutes);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'my-app.js'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 
