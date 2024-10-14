48526a65bffd2ad86ae0cead0cf1611e949c88a5
Mc Villalobos <mcstalin421@gmail.com>
2024-10-14 22:13:44 +0800

    ad

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  build: {
+    lib: {
+      entry: './src/main.jsx',
+      fileName: 'my-app',
+      formats: ['es'],
+    },
+  },
 })
