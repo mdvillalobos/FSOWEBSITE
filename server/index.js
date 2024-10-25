7af2c65c5fb78757e0c3edaf94dfca5f9be329a9
Mc Villalobos <mcstalin421@gmail.com>
2024-10-25 22:18:10 +0800

    asd

--- server/index.js
+++ server/index.js
@@ -88,9 +88,9 @@ app.use('/requirements', express.static(path.join(__dirname, 'requirements')));
 // router 
 app.use('/', apiRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, './client/dist', 'index.html'));
 });
 
 
